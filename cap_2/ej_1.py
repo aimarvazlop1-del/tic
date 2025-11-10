5320743efd3dca342052e28dc699b89ea28dc330
aimarvazlop1-del <aimar.vazlop1@gmail.com>
2025-11-10 13:13:00 +0100

    Add files via upload

---
+++ cap_2/ej_1.py
@@ -0,0 +1,7 @@
+num1 = float(input("Introduce el primer número: "))
+num2 = float(input("Introduce el segundo número: "))
+
+resultado = num1 * num2
+print("El resultado de la multiplicación es:", resultado)
+
+             
---
+++ cap_2/ej_6.py
@@ -0,0 +1,6 @@
+print("Programa que compara un número con el número 10")
+
+n = float(input("Escribe un número: "))
+
+print("¿El número", n, "es mayor o igual que 10?", n >= 10)
+print("¿El número", n, "es menor que 10?", n < 10)
