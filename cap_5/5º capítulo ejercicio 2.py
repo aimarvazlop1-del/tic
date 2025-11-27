d78eadebd42a9edd0e239ba83de02bc291456f16
juanmartin11 <juanmartingomez242@gmail.com>
2025-11-27 09:48:27 +0100

    Add files via upload

---
+++ cap_5/5º capítulo ejercicio 2.py
@@ -0,0 +1,6 @@
+import math
+print('Programa que calculará las raíces cuadradas de los números pares,')
+print('de los números: 1 al 100')
+for x in range(1,101):
+    if x % 2 == 0:
+        print(x,math.sqrt(x))
---
+++ cap_5/5º capítulo ejercicio 3.py
@@ -0,0 +1,6 @@
+import math
+print('Programa que calculará el logaritmo en base 10,')
+print('de los números: 1 al 100')
+for x in range(1,101):
+    print (x,math.log10(x))
+    
